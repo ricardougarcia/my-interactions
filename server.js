a897b63cbca55dcd19ab513eade500d44355bd7e
Ricardo Garcia <ricardougarcia@gmail.com>
2021-11-29 21:33:57 -0500

    t

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-// const hbs = exphbs.create();
+const hbs = exphbs.create();
 
 const sess = {
   secret: "Super Secret Password",
@@ -24,7 +24,7 @@ const sess = {
 };
 
 app.use(session(sess));
-const hbs = exphbs.create();
+// const hbs = exphbs.create();
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
