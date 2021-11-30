0ca62a844880a6824fcf090c06a4c261615eea79
Ricardo Garcia <ricardougarcia@gmail.com>
2021-11-29 20:05:45 -0500

    testing

--- public/js/login.js
+++ public/js/login.js
@@ -4,10 +4,12 @@ const loginFormHandler = async (event) => {
   // Collect values from the login form
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
+  console.log(email);
+  console.log(password);
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch("/api/users/login", {
+    const response = await fetch("/api/users", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
